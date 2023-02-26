1f31a435dab45a7ad3ddbd30598fe6101642d400
Gautham Sai <91204833+GauthamSSA@users.noreply.github.com>
2023-02-26 18:58:32 +0530

    Added html directly to php

--- Simple/login.php
+++ Simple/login.php
@@ -2,35 +2,91 @@
 
 session_start();
 
-// Check if the form was submitted
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-	// Validate the user's credentials
-	$username = $_POST['username'];
-	$password = $_POST['password'];
+	require_once('config.php');
 
-	// In a real-world scenario, you would need to validate the user's credentials against a database
-	// For simplicity, we'll just assume that the username is "user" and the password is "password"
-	if ($username == 'user' && $password == 'password') {
+	// Validate the user's input
+	$username = trim($_POST['username']);
+	$email = trim($_POST['email']);
+	$password = trim($_POST['password']);
 
-		// Set the user's session variables
-		$_SESSION['username'] = $username;
-
-		// Redirect the user to the dashboard
-		header('Location: dashboard.php');
-
-	} else {
-
-		// Redirect the user back to the login page with an error message
-		header('Location: login.php?login=failed');
-
+	// Check if the fields are empty or invalid
+	if (empty($username)) {
+		$_SESSION['error'] = 'Username is required';
+		header('Location: signup_form.html');
+		exit;
+	} else if (empty($email)) {
+		$_SESSION['error'] = 'Email is required';
+		header('Location: signup_form.html');
+		exit;
+	} else if (empty($password)) {
+		$_SESSION['error'] = 'Password is required';
+		header('Location: signup_form.html');
+		exit;
 	}
 
+	// Check if the email is already in use
+	$stmt = mysqli_prepare($conn, "SELECT id FROM users WHERE email = ?");
+	mysqli_stmt_bind_param($stmt, "s", $email);
+	mysqli_stmt_execute($stmt);
+	mysqli_stmt_store_result($stmt);
+
+	if (mysqli_stmt_num_rows($stmt) > 0) {
+		$_SESSION['error'] = 'Email is already in use';
+		header('Location: signup_form.html');
+		exit;
+	}
+
+	// Hash the password
+	$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+	// Insert the user's data into the database
+	$stmt = mysqli_prepare($conn, "INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
+	mysqli_stmt_bind_param($stmt, "sss", $username, $email, $hashed_password);
+	mysqli_stmt_execute($stmt);
+
+	$_SESSION['success'] = 'You have successfully signed up. Please log in.';
+	header('Location: login_form.html');
+
+	mysqli_stmt_close($stmt);
+	mysqli_close($conn);
+
 } else {
-
-	// Redirect the user back to the login page
-	header('Location: login.php');
-
+	// Redirect the user to the sign up page if they try to access this page directly
+	header('Location: signup_form.html');
+	exit;
 }
-
 ?>
+
+<!-- HTML code for signup_form.html -->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sign Up</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="container">
+		<h1>Sign Up</h1>
+		<?php
+			if (isset($_SESSION['error'])) {
+				echo '<div class="error">' . $_SESSION['error'] . '</div>';
+				unset($_SESSION['error']);
+			}
+		?>
+		<form method="POST" action="signup.php">
+			<label>Username:</label>
+			<input type="text" name="username" required><br>
+			<label>Email:</label>
+			<input type="email" name="email" required><br>
+			<label>Password:</label>
+			<input type="password" name="password" required><br>
+			<input type="submit" value="Sign Up">
+		</form>
+		<p>Already have an account? <a href="login_form.html">Log in</a></p>
+	</div>
+</body>
+</html>
+
